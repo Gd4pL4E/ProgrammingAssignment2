27855ea421dd97c468c964e9937763a0bfe4be62
Gd4pL4E <fvanbeveren@vmail.me>
2015-07-22 12:10:19 +0200

    first non-tested version of cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,36 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { 
+        #When the script is run, m must be reset (since u put in new data)
+        m <- NULL
+        #Instead, you can set new data by using x$set 
+        #Which also resets m (inverse needs to be calculated again for the new data).
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        #Return the currently in-use matrix (to be used by cacheSolve)
+        get <- function() x
+        #When setinverse is called, it will store the calculated inverse in m
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("showing cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
